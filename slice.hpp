8f10d2e364bd630d98bed8edc59c0089916d36f0
Dappur <dapperx@126.com>
2019-06-02 21:58:13 +0800

    Implement slice (some bugs remaining)

---
+++ slice.hpp
@@ -0,0 +1,100 @@
+#ifndef _SLICE_HPP_
+#define _SLICE_HPP_
+#include <cstdint>
+
+template<typename T>
+class slice{
+public:
+	class iterator{
+	public:
+		iterator(T base) : position(base){}
+		iterator(const iterator &rhs) : position(rhs.position){}
+		bool operator==(const iterator &rhs) const{
+			return position==rhs.position;
+		}
+		bool operator!=(const iterator &rhs) const{
+			return position!=rhs.position;
+		}
+		auto& operator*(){
+			return *position;
+		}
+		T& operator->(){
+			return position;
+		}
+		iterator& operator+=(int offset){
+			return position+=offset, *this;
+		}
+		iterator& operator++(){ // ++a
+			return ++position, *this;
+		}
+		iterator operator++(int){ // a++
+			return iterator(position++);
+		}
+		iterator operator+(int offset) const{
+			return iterator(position+offset);
+		}
+		iterator& operator-=(int offset){
+			return position-=offset, *this;
+		}
+		iterator& operator--(){
+			return ++position, *this;
+		}
+		iterator operator--(int){
+			return iterator(position++);
+		}
+		iterator operator-(int offset) const{
+			return iterator(position-offset);
+		}
+
+	protected:
+		T position;
+	};
+
+	class const_iterator : public iterator{
+	public:
+		const_iterator(T base) : iterator(base){}
+		const_iterator(const iterator &rhs) : iterator(rhs){}
+		const auto& operator*() const{
+			return *iterator::position;
+		}
+		const T& operator->() const{
+			return iterator::position;
+		}
+		const_iterator operator+(int offset) const{
+			return const_iterator(iterator::position+offset);
+		}
+		const_iterator operator-(int offset) const{
+			return const_iterator(iterator::position-offset);
+		}
+	};
+
+	slice(const T& begin, size_t n)
+		: base(begin), length(n) {}
+	iterator begin(){
+		return iterator(base);
+	}
+	const_iterator begin() const{
+		return iterator(base);
+	}
+	iterator end(){
+		return iterator(base+length);
+	}
+	const_iterator end() const{
+		return iterator(base+length);
+	}
+	size_t size() const{
+		return length;
+	}
+	auto& operator[](const int offset){
+		return *(base+offset);
+	}
+	const auto& operator[](const int offset) const{
+		return *(base+offset);
+	}
+
+private:
+	const T base;
+	const size_t length;
+};
+
+#endif // _SLICE_HPP_
